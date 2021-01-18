f22079e33195ce690f3d82d0d298277f19bd1091
ayoubkandah <a.kandah994@gmail.com>
2021-01-18 16:29:12 +0200

    add chart

--- js/app.js
+++ js/app.js
@@ -8,11 +8,16 @@ var trigger=true;
 var image1=document.getElementById("image1")
 var image2=document.getElementById("image2")
 var image3=document.getElementById("image3")
-
+var nameG=[];
+var voteG=[];
+var numSh=[];
 var Div=document.getElementById("Section")
 
 var num = document.getElementById("maxnum")
 var sub = document.getElementById("formID");
+var iamgeprev1=99;
+var iamgeprev2=99;
+var iamgeprev3=99;
 
  sub.addEventListener("submit", submitRun);
 
@@ -31,6 +36,7 @@ function allimages(name,source){
     this.timesshown=0;
     this.vote=0
     allimages.prototype.inserti.push(this)
+    nameG.push(name)
 }
 allimages.prototype.inserti=[];
 
@@ -69,10 +75,14 @@ function randomimage(){
   {  image1index=getrandomnumber();
     image2index=getrandomnumber();
     image3index=getrandomnumber();}
-    while(image1index==image2index||image3index==image1index||image2index==image3index)
+    while(image1index==image2index||image3index==image1index||image2index==image3index ||iamgeprev1==image1index || iamgeprev2==image1index || iamgeprev3==image1index || iamgeprev3==image2index || iamgeprev2==image2index || iamgeprev3==image3index ||iamgeprev1==image1index || iamgeprev2==image1index || iamgeprev3==image1index || iamgeprev3==image2index || iamgeprev2==image2index || iamgeprev3==image3index)
 // console.log(allimages.prototype.inserti[image2index].source)
+iamgeprev1=image1index;
+iamgeprev2=image2index;
+iamgeprev3=image3index;
 image1.src=allimages.prototype.inserti[image1index].source
 allimages.prototype.inserti[image1index].timesshown++;
+
 console.log(allimages.prototype.inserti[image1index].timesshown);
 
 image2.src=allimages.prototype.inserti[image2index].source
@@ -117,6 +127,7 @@ randomimage();
 }
 
 function Result(){
+    if(startsel>=maxsel){
     if (trigger){
 var percent;
     var ULE=document.getElementById("UL")
@@ -127,14 +138,78 @@ var percent;
 percent=0;
         } else {
             percent=allimages.prototype.inserti[x].vote/allimages.prototype.inserti[x].timesshown;
-        
+        voteG.push(allimages.prototype.inserti[x].vote)
+        numSh.push(allimages.prototype.inserti[x].timesshown)
             percent=Math.floor(percent*100);
 
         }
         ListE.textContent="Name "+ allimages.prototype.inserti[x].name + " Votes " + allimages.prototype.inserti[x].vote + " Times Shown " + allimages.prototype.inserti[x].timesshown + " Percentage "+ percent+"%"
         ULE.appendChild(ListE)
     }
+    chartRes()
     trigger=false;
 }
+}else{alert("You still have  "+(maxsel-startsel)  +"  Votes | you can edit number of selection")}
+}
+
+// console.log(nameG)
+function chartRes(){
+var ctx=document.getElementById("Canvas").getContext('2d');
+var mixedChart  = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        
+        labels:nameG,
+       
+        datasets: [{
+            
+            label:"Votes",
+            backgroundColor:"    rgba(96, 204, 33, 0.733)",
+            hoverBackgroundColor:"rgba(2, 255, 221, 1)",
+       
+            barPercentage: 1,
+            barThickness: 50,
+            maxBarThickness: 50,
+            minBarLength: 2,
+            order: 1,
+            data: voteG
+    
+         
+        },
+        {  
+            order:2,
+            type:"line",
+            label:"Times Of Shown",
+            pointBorderWidth:2,
+        backgroundColor:"   rgba(54, 99, 110, 0.849)  ",
+        hoverBackgroundColor:"rgba(2, 255, 221, 1)",
+        borderWidth:9,
+        // barPercentage: 0.5,
+        // barThickness: 15,
+        // maxBarThickness: 15,
+        // minBarLength: 2,
+     data:num.length,
+        data: numSh
+
+     
+        
+       
+
+        
+  
+    }],
+},
+    
+    // options: {
+    //     scales: {
+    //         xAxes: [{
+    //             stacked: true
+    //         }],
+    //         yAxes: [{
+    //             stacked: true
+    //         }]
+    //     }
+    // }
+});
 }
 
