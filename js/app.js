9513d71c0de287bba63736d0025680db2a68ea90
ayoubkandah <a.kandah994@gmail.com>
2021-01-19 18:05:18 +0200

    lab 13

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@ var nameG=[];
 var voteG=[];
 var numSh=[];
 var Div=document.getElementById("Section")
-
+var triggersell=false;
 var num = document.getElementById("maxnum")
 var sub = document.getElementById("formID");
 var iamgeprev1=99;
 var iamgeprev2=99;
 var iamgeprev3=99;
-
+var prevTrigger=false;
  sub.addEventListener("submit", submitRun);
 
  function submitRun (event){
@@ -71,53 +71,45 @@ function getrandomnumber(){
 
 function randomimage(){
 
-    
   do
   {  image1index=getrandomnumber();
     image2index=getrandomnumber();
-    image3index=getrandomnumber();
-    while(image1index==iamgeprev1 || image1index==iamgeprev2 || image1index==iamgeprev3 ||image2index== iamgeprev3||image2index== iamgeprev2 || image3index==iamgeprev3)
-    {
-        image1index=getrandomnumber();
-        image2index=getrandomnumber();
-        image3index=getrandomnumber();  
-    }
-}
-    while(image1index==image2index||image3index==image1index||image2index==image3index  )
+    image3index=getrandomnumber();}
+    while(image1index==image2index||image3index==image1index||image2index==image3index ||iamgeprev1==image1index || iamgeprev2==image1index || iamgeprev3==image1index || iamgeprev3==image2index || iamgeprev2==image2index || iamgeprev3==image3index ||iamgeprev1==image1index || iamgeprev2==image1index || iamgeprev3==image1index || iamgeprev3==image2index || iamgeprev2==image2index || iamgeprev3==image3index)
 // console.log(allimages.prototype.inserti[image2index].source)
-if( image1index==iamgeprev1 || image1index==iamgeprev2 || image1index==iamgeprev3 ||image2index== iamgeprev3||image2index== iamgeprev2 || image3index==iamgeprev3)
-{
-    console.log("s")
-  
-
-}
+iamgeprev1=image1index;
+iamgeprev2=image2index;
+iamgeprev3=image3index;
 image1.src=allimages.prototype.inserti[image1index].source
 allimages.prototype.inserti[image1index].timesshown++;
-
+console.log(image1index,image2index,image3index)
+console.log(allimages.prototype.inserti[image1index].timesshown);
 
 image2.src=allimages.prototype.inserti[image2index].source
 allimages.prototype.inserti[image2index].timesshown++;
 image3.src=allimages.prototype.inserti[image3index].source
 allimages.prototype.inserti[image3index].timesshown++;
-console.log(image3index,image2index,image1index);
-iamgeprev1=image1index;
-iamgeprev2=image2index;
-iamgeprev3=image3index;
-console.log();
+
+
 }
 
-randomimage(image1index,image2index,image3index)
+randomimage()
 
 // image1.addEventListener("click",render)
 // image2.addEventListener("click",render)
 // image3.addEventListener("click",render)
 
+if (localStorage.getItem("Clicked")>0){
+    var storedclick=localStorage.getItem("Clicked");
+}else{var storedclick=0}
 Div.addEventListener("click",render)
 function render(event){
    
-startsel++;
 if(startsel<=maxsel){
-    
+    triggersell=true;
+    storedclick++;
+    localStorage.setItem("Clicked",storedclick)
+    startsel=+localStorage.getItem("Clicked")
 if(event.target.id=="image1"){
     allimages.prototype.inserti[image1index].vote++;
   
@@ -138,28 +130,81 @@ if(startsel<=maxsel){
 randomimage();
 }
 }
+ if(!localStorage.getItem("Votes")){
+ var prevVotes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+ var prevShown=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+ var prevNames=[];
+ console.log("SS");
+ console.log(prevShown);
+
+ }else
+ {
+    prevTrigger=true;
+ // localStorage.clear()
+     var prevVotes =JSON.parse(localStorage.getItem("Votes"))
+     var prevShown=JSON.parse(localStorage.getItem("Shown"))
+    // var prevNames=JSON.parse(localStorage.getItem("Names"))
+
+    // var prevNames=localStorage.getItem("Names")
+    // JSON.parse(prevNames)
+    console.log("SA");
+    console.log(prevShown);
+
+ }
 
 function Result(){
+  
+
+    if(!triggersell){
+    startsel=+localStorage.getItem("Clicked")
+    triggersell=true;
+    }
     if(startsel>=maxsel){
     if (trigger){
+       
+
 var percent;
     var ULE=document.getElementById("UL")
    
     for( var x=0;x<allimages.prototype.inserti.length;x++){
+
         var ListE=document.createElement("li")
-        if(allimages.prototype.inserti[x].timesshown==0 && allimages.prototype.inserti[x].vote==0){
+        if((allimages.prototype.inserti[x].timesshown+prevShown[x])==0 && (allimages.prototype.inserti[x].vote==0+prevVotes[x])){
 percent=0;
         } else {
-            percent=allimages.prototype.inserti[x].vote/allimages.prototype.inserti[x].timesshown;
-        voteG.push(allimages.prototype.inserti[x].vote)
-        numSh.push(allimages.prototype.inserti[x].timesshown)
-            percent=Math.floor(percent*100);
+
+            percent=(allimages.prototype.inserti[x].vote+prevVotes[x])/(allimages.prototype.inserti[x].timesshown+prevShown[x]);
+          
+
+   // localStorage.setItem("object",allimages)
+
+        percent=Math.floor(percent*100);
 
         }
-        ListE.textContent="Name "+ allimages.prototype.inserti[x].name + " Votes " + allimages.prototype.inserti[x].vote + " Times Shown " + allimages.prototype.inserti[x].timesshown + " Percentage "+ percent+"%"
+        voteG.push(allimages.prototype.inserti[x].vote)
+        numSh.push(allimages.prototype.inserti[x].timesshown)
+        
+        prevVotes[x]=(prevVotes[x]+allimages.prototype.inserti[x].vote)
+        prevShown[x]=(prevShown[x]+allimages.prototype.inserti[x].timesshown)   
+        console.log(prevShown);
+//   prevVotes.push(allimages.prototype.inserti[x].vote)
+  //prevShown.push(allimages.prototype.inserti[x].timesshown)
+// prevNames.push(allimages.prototype.inserti[x].name)
+        ListE.textContent="Name "+ allimages.prototype.inserti[x].name + " Votes " + prevVotes[x] + " Times Shown " + prevShown[x] + " Percentage "+ percent+"%"
         ULE.appendChild(ListE)
     }
+
+console.log(prevShown);
+    localStorage.setItem("Votes",JSON.stringify(prevVotes))
+    localStorage.setItem("Shown",JSON.stringify(prevShown))
+    //localStorage.setItem("Names",JSON.stringify(prevNames))
+
     chartRes()
+    startsel=+maxsel+1;
+    storedclick =0;
+    localStorage.setItem("Clicked",storedclick)
+
+
     trigger=false;
 }
 }else{alert("You still have  "+(maxsel-startsel)  +"  Votes | you can edit number of selection")}
@@ -213,16 +258,113 @@ var mixedChart  = new Chart(ctx, {
     }],
 },
     
-    // options: {
-    //     scales: {
-    //         xAxes: [{
-    //             stacked: true
-    //         }],
-    //         yAxes: [{
-    //             stacked: true
-    //         }]
-    //     }
-    // }
+    options: {
+         scales: {
+            xAxes: [{
+                stacked: true
+            }],
+           yAxes: [{
+                stacked: true
+            }]
+        }
+     }
 });
 }
 
+
+
+// function PrevResult(){
+
+// if(prevTrigger){
+//     if (prevShown.length>0){
+       
+// console.log("dd");
+// var percent;
+//     var ULE=document.getElementById("UL")
+   
+//     for( var x=0;x<prevNames.length;x++){
+//         var ListE=document.createElement("li")
+//         if(prevShown[x]==0 && prevVotes==0){
+// percent=0;
+//         } else {
+//             percent=prevVotes[x]/prevShown[x];
+         
+          
+//         percent=Math.floor(percent*100);
+
+//         }
+//         ListE.textContent="Name "+ prevNames[x]+ " Votes " + prevVotes[x] + " Times Shown " + prevShown[x] + " Percentage "+ percent+"%"
+//         ULE.appendChild(ListE)
+//     }
+//     PrevchartRes()
+  
+
+
+ 
+// }
+// }
+// else{alert("you dont have any previous Vote")}
+// }
+
+
+// function PrevchartRes(){
+//     var ctx=document.getElementById("Canvas").getContext('2d');
+//     var mixedChart  = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+            
+//             labels:nameG,
+           
+//             datasets: [{
+                
+//                 label:"Prev Votes",
+//                 backgroundColor:"    rgba(96, 204, 33, 0.733)",
+//                 hoverBackgroundColor:"rgba(2, 255, 221, 1)",
+           
+//                 barPercentage: 1,
+//                 barThickness: 50,
+//                 maxBarThickness: 50,
+//                 minBarLength: 2,
+//                 order: 1,
+//                 data: prevVotes
+        
+             
+//             },
+//             {  
+//                 order:2,
+//                 type:"line",
+//                 label:"Prev Times Of Shown",
+//                 pointBorderWidth:2,
+//             backgroundColor:"   rgba(54, 99, 110, 0.849)  ",
+//             hoverBackgroundColor:"rgba(2, 255, 221, 1)",
+//             borderWidth:9,
+//             // barPercentage: 0.5,
+//             // barThickness: 15,
+//             // maxBarThickness: 15,
+//             // minBarLength: 2,
+//          data:num.length,
+//             data: prevShown
+    
+         
+            
+           
+    
+            
+      
+//         }],
+//     },
+        
+//         options: {
+//              scales: {
+//                 xAxes: [{
+//                     stacked: true
+//                 }],
+//                yAxes: [{
+//                     stacked: true
+//                 }]
+//             }
+//          }
+//     });
+//     }
+    
+    
